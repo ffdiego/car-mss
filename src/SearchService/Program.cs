c95351cf6a3257b0073bc9937dad4db9c8d61527
Diego Fernandes <diego.silva@luz-ef.com>
2024-05-25 10:30:36 -0400

    [SearchService] Inicializa com mongo

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,8 +1,25 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 
 var app = builder.Build();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
