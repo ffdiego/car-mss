c5e176658823557c3d28264375d12369bdf1af18
Diego Fernandes <diego.silva@luz-ef.com>
2024-05-27 16:06:48 -0400

    [SearchService] HTTP Policy para obtenção de dados

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,26 +1,39 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+
+builder.Services
+    .AddHttpClient<AuctionServiceHttpClient>()
+    .AddPolicyHandler(
+        HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+            .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3))
+    );
 
 var app = builder.Build();
-
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () => 
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
 
 app.Run();
